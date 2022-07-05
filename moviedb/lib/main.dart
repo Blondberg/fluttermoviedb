c7547a31842e7e47494410d91491c50b5da28cd0
Simon Broborg <iamblondberg@gmail.com>
2022-07-05 09:22:00 +0200

    yes

--- moviedb/lib/main.dart
+++ moviedb/lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:moviedb/screens/googlesignin/signin_screen.dart';
-import 'package:moviedb/screens/navbar/navbar_screen.dart';
+import 'package:moviedb/screens/button_w_popup/buttonwpopup.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Movie database',
-      home: SignInScreen(),
+      home: const ButtonWPopup(),
       theme: ThemeData(
         scaffoldBackgroundColor: const Color(0xFFFAFAFA),
         visualDensity: VisualDensity.adaptivePlatformDensity,
---
+++ moviedb/lib/screens/button_w_popup/buttonwpopup.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ButtonWPopup extends StatefulWidget {
+  const ButtonWPopup({Key? key}) : super(key: key);
+
+  @override
+  State<ButtonWPopup> createState() => _ButtonWPopupState();
+}
+
+class _ButtonWPopupState extends State<ButtonWPopup> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
